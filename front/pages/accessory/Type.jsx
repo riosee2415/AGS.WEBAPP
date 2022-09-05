638bc4dedecee4c1ded4f9fe0dc3152f6b18296e
4leaf.peb <4leaf.peb@gmail.com>
2022-09-05 17:56:35 +0900

    modfiy

---
+++ front/pages/accessory/Type.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect } from "react";
+import ClientLayout from "../../components/ClientLayout";
+import { useDispatch, useSelector } from "react-redux";
+
+import wrapper from "../../store/configureStore";
+import { END } from "redux-saga";
+import useWidth from "../../hooks/useWidth";
+import useInput from "../../hooks/useInput";
+import Theme from "../../components/Theme";
+import styled from "styled-components";
+import axios from "axios";
+
+import { LOAD_MY_INFO_REQUEST } from "../../reducers/user";
+import { SEO_LIST_REQUEST } from "../../reducers/seo";
+
+import Head from "next/head";
+
+const Type = () => {
+  ////// GLOBAL STATE //////
+
+  ////// HOOKS //////
+  ////// REDUX //////
+  ////// USEEFFECT //////
+  ////// TOGGLE //////
+  ////// HANDLER //////
+  ////// DATAVIEW //////
+
+  return (
+    <>
+      <Head>
+        <title>AGS | 폼캐논&#38;렌스</title>
+      </Head>
+
+      <ClientLayout></ClientLayout>
+    </>
+  );
+};
+
+export const getServerSideProps = wrapper.getServerSideProps(
+  async (context) => {
+    // SSR Cookie Settings For Data Load/////////////////////////////////////
+    const cookie = context.req ? context.req.headers.cookie : "";
+    axios.defaults.headers.Cookie = "";
+    if (context.req && cookie) {
+      axios.defaults.headers.Cookie = cookie;
+    }
+    ////////////////////////////////////////////////////////////////////////
+    // 구현부
+
+    context.store.dispatch({
+      type: LOAD_MY_INFO_REQUEST,
+    });
+
+    // 구현부 종료
+    context.store.dispatch(END);
+    console.log("🍀 SERVER SIDE PROPS END");
+    await context.store.sagaTask.toPromise();
+  }
+);
+
+export default Type;
