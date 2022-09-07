1a0ecf7cf0c55e9a17e5845bf1042900915ab5ae
4LEAF_DEV_MKH <mkh0169@gmail.com>
2022-09-07 19:18:39 +0900

    modify

--- front/pages/center/faq.jsx
+++ front/pages/center/faq.jsx
@@ -7,19 +7,15 @@ import { END } from "redux-saga";
 import axios from "axios";
 import { DownOutlined, UpOutlined } from "@ant-design/icons";
 
-import styled from "styled-components";
 import ClientLayout from "../../components/ClientLayout";
 import Theme from "../../components/Theme";
 import wrapper from "../../store/configureStore";
 import useWidth from "../../hooks/useWidth";
-import useInput from "../../hooks/useInput";
 
 import {
-  CommonButton,
   Image,
   RsWrapper,
   Text,
-  TextInput,
   Title,
   WholeWrapper,
   Wrapper,
@@ -27,14 +23,15 @@ import {
 
 import { LOAD_MY_INFO_REQUEST } from "../../reducers/user";
 import { FAQ_LIST_REQUEST } from "../../reducers/faq";
+import { Empty } from "antd";
 
 const Faq = () => {
   ////// GLOBAL STATE //////
   const { faqs } = useSelector((state) => state.faq);
   const [tab, setTab] = useState("");
+
   ////// HOOKS //////
   const width = useWidth();
-
   const dispatch = useDispatch();
   const router = useRouter();
 
@@ -52,11 +49,7 @@ const Faq = () => {
     },
     [tab]
   );
-  ////// HANDLER //////'
-  const moveLinkHandler = useCallback((link) => {
-    router.push(link);
-  }, []);
-
+  ////// HANDLER ///////
   ////// DATAVIEW //////
 
   return (
@@ -73,83 +66,89 @@ const Faq = () => {
             </Wrapper>
 
             <Wrapper margin={`0 0 100px`}>
-              <>
-                {faqs &&
-                  faqs.map((data, idx) => {
-                    return (
-                      <Wrapper
-                        height={data.id === tab ? `140px` : `70px`}
-                        bgColor={Theme.black3_C}
-                        radius={`10px`}
-                        margin={`0 0 25px`}
-                        padding={`20px`}
-                        cursor={`pointer`}
-                        isActive={data.id === tab ? true : false}
-                        onClick={() => tabHandler(data.id)}
-                        key={idx}
-                      >
-                        <Wrapper dr={`row`}>
-                          <Image
-                            src={`https://4leaf-s3.s3.ap-northeast-2.amazonaws.com/ags/assets/images/customer-support/icon_faq-question.png`}
-                            width={`24px`}
-                            height={`24px`}
-                          />
-                          <Wrapper
-                            dr={`row`}
-                            ju={`flex-start`}
-                            width={`calc(100% - 55px)`}
-                          >
-                            <Text
-                              fontSize={`20px`}
-                              fontWeight={`700`}
-                              color={
-                                data.id === tab
-                                  ? Theme.basicTheme_C
-                                  : Theme.white_C
-                              }
-                              margin={`0 0 0 20px`}
-                            >
-                              {data.question}
-                            </Text>
-                          </Wrapper>
-                          {data.id === tab ? (
+              {faqs.length === 0 ? (
+                <Wrapper height={`200px`}>
+                  <Empty description="FAQ가 준비중입니다." />
+                </Wrapper>
+              ) : (
+                <>
+                  {faqs &&
+                    faqs.map((data, idx) => {
+                      return (
+                        <Wrapper
+                          height={data.id === tab ? `140px` : `70px`}
+                          bgColor={Theme.black3_C}
+                          radius={`10px`}
+                          margin={`0 0 25px`}
+                          padding={`20px`}
+                          cursor={`pointer`}
+                          isActive={data.id === tab ? true : false}
+                          onClick={() => tabHandler(data.id)}
+                          key={idx}
+                        >
+                          <Wrapper dr={`row`}>
+                            <Image
+                              src={`https://4leaf-s3.s3.ap-northeast-2.amazonaws.com/ags/assets/images/customer-support/icon_faq-question.png`}
+                              width={`24px`}
+                              height={`24px`}
+                            />
                             <Wrapper
-                              width={`20px`}
-                              fontSize={`18px`}
-                              al={`flex-end`}
+                              dr={`row`}
                               ju={`flex-start`}
-                              color={Theme.white_C}
+                              width={`calc(100% - 55px)`}
                             >
-                              <UpOutlined />
+                              <Text
+                                fontSize={`20px`}
+                                fontWeight={`700`}
+                                color={
+                                  data.id === tab
+                                    ? Theme.basicTheme_C
+                                    : Theme.white_C
+                                }
+                                margin={`0 0 0 20px`}
+                              >
+                                {data.question}
+                              </Text>
                             </Wrapper>
-                          ) : null}
+                            {data.id === tab ? (
+                              <Wrapper
+                                width={`20px`}
+                                fontSize={`18px`}
+                                al={`flex-end`}
+                                ju={`flex-start`}
+                                color={Theme.white_C}
+                              >
+                                <UpOutlined />
+                              </Wrapper>
+                            ) : null}
 
-                          {data.id === tab ? (
-                            <Wrapper
-                              al={`flex-start`}
-                              padding={`20px 50px 0`}
-                              fontSize={`16px`}
-                              color={Theme.lightGrey2_C}
-                            >
-                              <Text>{data.answer}</Text>
-                            </Wrapper>
-                          ) : null}
+                            {data.id === tab ? (
+                              <Wrapper
+                                al={`flex-start`}
+                                padding={`20px 50px 0`}
+                                fontSize={`16px`}
+                                color={Theme.lightGrey2_C}
+                              >
+                                <Text>{data.answer}</Text>
+                              </Wrapper>
+                            ) : null}
 
-                          {data.id === tab ? null : (
-                            <Wrapper
-                              al={`flex-end`}
-                              width={`20px`}
-                              fontSize={`18px`}
-                              color={Theme.white_C}
-                            >
-                              <DownOutlined />
-                            </Wrapper>
-                          )}
+                            {data.id === tab ? null : (
+                              <Wrapper
+                                al={`flex-end`}
+                                width={`20px`}
+                                fontSize={`18px`}
+                                color={Theme.white_C}
+                              >
+                                <DownOutlined />
+                              </Wrapper>
+                            )}
+                          </Wrapper>
                         </Wrapper>
-                      </Wrapper>
-                    );
-                  })}
-              </>
+                      );
+                    })}
+                </>
+              )}
             </Wrapper>
           </RsWrapper>
         </WholeWrapper>
